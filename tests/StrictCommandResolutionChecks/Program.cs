05d4c7e69823ab2fcfa229a80e6375a9728f993a
EltonZhang777 <280903158@qq.com>
2026-09-14 22:35:37 +0800

    fix(console): honor whitespace contract
    Keep leading, trailing, and repeated whitespace harmless as required by the strict resolution specification. Refs #12

--- tests/StrictCommandResolutionChecks/Program.cs
+++ tests/StrictCommandResolutionChecks/Program.cs
@@ -28,9 +28,8 @@ internal static class Program
         Check(string.Join(",", StrictCommandResolver.GetSuggestions("ba", commands, 1)) == "ab");
         Check(StrictCommandResolver.NormalizeDistance(-1) == 0);
         Check(new MasterConfig().CommandSuggestionDistance == 2);
-        Check(string.Join("/", StrictCommandResolver.Tokenize("AUDIO  master")) == "AUDIO//master");
-        Check(StrictCommandResolver.Tokenize("AUDIO\tmaster")[0] == "AUDIO\tmaster");
-        Check(StrictCommandResolver.Tokenize("clear ").Length == 2);
+        Check(string.Join("/", StrictCommandResolver.Tokenize("  AUDIO\t master  ")) == "AUDIO/master");
+        Check(StrictCommandResolver.Tokenize("clear ").Length == 1);
         Check(StrictCommandResolver.FindExactSubcommand(commands[2], "MASTER") == "master");
         Check(StrictCommandResolver.FindExactSubcommand(commands[2], "mas") == null);
         Check(string.Join(",", StrictCommandResolver.GetSubcommandSuggestions(commands[2], "mastr", 1)) == "audio master");
